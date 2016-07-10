3d083445b6bd4f76a612cfed12cd02abe0fe0fbb
lixiaojing <lixiaojing@etaostars.com>
2016-07-10 15:20:50 +0800

    提交文件

--- test/EntityPropertyHelperTest.cs
+++ test/EntityPropertyHelperTest.cs
@@ -8,6 +8,8 @@ namespace ConsoleApplication
             var p =new Easy.Public.EntityPropertyHelper<Entity>();
             var entity =new Entity();
             p.SetValue(m=>m.Id,entity,100);
+
+            System.Console.WriteLine($"should 100 acutal {entity.Id}");
         }
     }
 
--- test/Program.cs
+++ test/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Easy.Public;
+using System;
 
 namespace ConsoleApplication
 {
@@ -6,8 +7,36 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var test = new ConsoleApplication.EntityPropertyHelperTest();
+            var test = new EntityPropertyHelperTest();
+            test.SetPropertyTest();
+            Entity entity = null;
 
+            var result = NullHelper.IfNull(entity, 100, m => m.Id);
+            Console.WriteLine($"should 100 actual {result}");
+
+            var q = new Query()
+            {
+                Name = "lixiaojing",
+                Id = 0
+            };
+
+            var sql = new SQLBuilder();
+            sql.AppendWhere();
+            sql.Append(!string.IsNullOrWhiteSpace(q.Name), "and", "name=@name");
+            sql.Append(q.Id>0,"and","Id=@Id");
+
+            Console.WriteLine(sql.Sql());
+
+            Console.WriteLine("外网IP：" + IpHelper.InternetIp4()); 
+            Console.WriteLine("内网IP：" + IpHelper.IntranetIp4());
+            Console.WriteLine("look up：" + IpHelper.LoopbackIp());
+            
         }
+    }
+
+    class Query
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
     }
 }
